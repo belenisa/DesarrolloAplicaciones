175574439ddb5249b4755c480f10787304fca11f
belenisa <bel.alarconh@duocuc.cl>
2025-10-17 21:19:28 -0300

    2

--- evaluacion 2/app/src/main/java/com/example/evaluacion2/MainActivity.kt
+++ evaluacion 2/app/src/main/java/com/example/evaluacion2/MainActivity.kt
@@ -14,12 +14,10 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.evaluacion2.Data.CatalogoCDs
 import com.example.evaluacion2.ui.theme.Evaluacion2Theme
