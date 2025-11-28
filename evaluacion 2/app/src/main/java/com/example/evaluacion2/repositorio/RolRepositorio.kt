30bdfba9737e13fa52964648b8f575987fc87778
belenisa <bel.alarconh@duocuc.cl>
2025-11-28 15:43:18 -0300

    8

--- evaluacion 2/app/src/main/java/com/example/evaluacion2/repositorio/RolRepositorio.kt
+++ evaluacion 2/app/src/main/java/com/example/evaluacion2/repositorio/RolRepositorio.kt
@@ -1,10 +1,23 @@
 package com.example.evaluacion2.repositorio
 
-import com.example.evaluacion2.Data.network.Rol.RolApi
+import com.example.evaluacion2.Data.Modelo.Rol
 import com.example.evaluacion2.Data.network.Rol.RolService
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import retrofit2.HttpException
+import java.io.IOException
+
 
 class RolRepositorio(
-    private val service: RolService = RolApi.retrofit.create(RolService::class.java)
-)
-{
+    private val service: RolService
+) {
+    suspend fun listarRoles(): List<Rol>? {
+        val response = service.listar()
+        return if (response.isSuccessful) response.body() else null
+    }
+
+    suspend fun obtenerRol(id: Int): Rol? {
+        val response = service.obtener(id)
+        return if (response.isSuccessful) response.body() else null
+    }
 }
